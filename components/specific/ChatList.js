2d4fe6d92e25c8dfebedb9a720dc16d4c11cef95
Rohan-Lohiya <rohanhero182004@gmail.com>
2025-05-07 16:16:20 +0530

    Add files via upload

---
+++ components/specific/ChatList.js
@@ -0,0 +1,67 @@
+"use client";
+import React, { useState } from "react";
+import styles from "./chatlist.module.css";
+import Image from "next/image";
+import { useDispatch, useSelector } from "react-redux";
+import { setSelectedGoogleID } from "@/app/store/selectedUserSlice";
+import { useEffect } from "react";
+
+const ChatList = ({ totalpeople = [{}], messages = [] }) => {
+  const dispatch = useDispatch();
+  const selectedGoogleID = useSelector((state) => state.selectedUser.googleID);
+
+  useEffect(() => {
+    if (selectedGoogleID) {
+      console.log("Selected Google ID:", selectedGoogleID);
+    }
+  }, [selectedGoogleID]);
+
+  const handleClick = (googleID) => {
+    dispatch(setSelectedGoogleID(googleID));
+  };
+
+  const getUnreadCount = (googleID) => {
+    return messages.filter(
+      (msg) => msg.from === googleID && !msg.read
+    ).length;
+  };
+
+  return (
+    <div className={styles.chatContainer}>
+      {totalpeople.map((friend, index) => {
+        const unreadCount = getUnreadCount(friend.GoogleID);
+
+        return (
+          <div
+            key={index}
+            className={`${styles.chatItem} ${
+              selectedGoogleID === friend.GoogleID ? styles.isselected : ""
+            }`}
+            onClick={() => handleClick(friend.GoogleID)}
+          >
+            <span className={styles.avatar}>
+              <Image
+                src="/chad.jpeg"
+                height={40}
+                width={40}
+                alt={friend.Name}
+              />
+            </span>
+            <span className={styles.chatInfo}>
+              <div className={styles.friendName}>{friend.Name}</div>
+              <div className={styles.newMessages}>
+                {unreadCount > 0
+                  ? `${unreadCount} new message(s)`
+                  : "No new messages"}
+              </div>
+            </span>
+            <hr className={styles.linebreak}/>
+          </div>
+          
+        );
+      })}
+    </div>
+  );
+};
+
+export default ChatList;
